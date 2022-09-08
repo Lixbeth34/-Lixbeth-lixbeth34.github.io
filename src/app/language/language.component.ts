507fe66fb8cbef2bd34ef91c6eaed8c38c1fdf24
Lixbeth34 <lixbethcaballero02@gmail.com>
2022-09-08 04:26:49 -0500

    add branch language - Closes #11

--- src/app/language/language.component.ts
+++ src/app/language/language.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LanguageComponent implements OnInit {
 
+  workExperience : Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
-  }
+	let work1 = {
+		language: "Ingles"
+	};
 
+	let work2 = {
+                language: "español"
+           
+        };
+
+	this.workExperience.push(work1);
+	this.workExperience.push(work2);
+	}
 }
