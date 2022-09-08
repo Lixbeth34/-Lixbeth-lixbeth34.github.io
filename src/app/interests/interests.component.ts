f5a08d602a0a2cd056d1584abdb5373b319b7ad2
Lixbeth34 <lixbethcaballero02@gmail.com>
2022-09-08 04:48:05 -0500

    add interest Closes #12

--- src/app/interests/interests.component.ts
+++ src/app/interests/interests.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InterestsComponent implements OnInit {
 
+  workExperience : Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
-  }
+	let work1 = {
+		ints: "musica"
+	};
 
+	let work2 = {
+                ints: "dibujo"
+
+        };
+
+	let work3 = {
+                ints: "baile"
+        };
+
+	this.workExperience.push(work1);
+	this.workExperience.push(work2);
+	this.workExperience.push(work3);
+
+  }
 }
