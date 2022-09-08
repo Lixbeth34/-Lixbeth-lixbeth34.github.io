f423c897600dc4f267c1b64332e63f43a2f06cf2
Lixbeth34 <lixbethcaballero02@gmail.com>
2022-09-07 23:24:57 -0500

    create work_experience #2

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkExperienceComponent implements OnInit {
 
+  workExperience : Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
-  }
+	let work1 = {
+		fecha: "2014-2015",
+		ubicacion: "Córdoba, Ver",
+		puesto:"Atención a clientes",
+		empresa: "La torre",
+		logros: [
+			{descripcion: "Jefa de departamento"}
+		]
+	};
 
+	let work2 = {
+                fecha: "2016-2018",
+                ubicacion: "Córdoba, Ver",
+                puesto:"Ayudante auxiliar de Centro de Copiados",
+                empresa: "Papeleria del CBTIS",
+		logros: [
+                        {descripcion: "Asistencia"}
+		]
+
+        };
+
+	let work3 = {
+                fecha: "2020-2021",
+                ubicacion: "Córdoba, Ver",
+                puesto:"Mesera",
+                empresa: "Salón de fiesta la Finca San Juan",
+		logros: [
+                        {descripcion: "Jefe de meseros"}
+		]
+
+        };
+
+	this.workExperience.push(work1);
+	this.workExperience.push(work2);
+	this.workExperience.push(work3);
+
+  }
 }
