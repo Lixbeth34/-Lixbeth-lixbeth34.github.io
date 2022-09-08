6930e46f169773ad872246932d80b66a1089a115
Lixbeth34 <lixbethcaballero02@gmail.com>
2022-09-08 02:51:49 -0500

    add competences - Closes #9

--- src/app/competences/competences.component.ts
+++ src/app/competences/competences.component.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompetencesComponent implements OnInit {
 
+  workExperience : Array<any> = [];
   constructor() { }
 
   ngOnInit(): void {
-  }
+	let work1 = {
+		tema: "angular",
+		line: "______________________"
+	};
 
+	let work2 = {
+                tema: "java",
+                line: "__________________"
+        };
+
+	let work3 = {
+                tema: "python",
+                line: "__________________________",
+        };
+
+	this.workExperience.push(work1);
+	this.workExperience.push(work2);
+	this.workExperience.push(work3);
+
+  }
 }
